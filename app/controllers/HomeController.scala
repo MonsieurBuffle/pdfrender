760f267d0dbe9fe0ec8ed3e08b697d8a4a0dd7e9
Phivan Le Guidec <phivan.leguidec@valraiso.fr>
2016-09-09 16:48:00 +0200

    init scala sources

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -17,8 +17,29 @@ class HomeController @Inject() extends Controller {
    * will be called when the application receives a `GET` request with
    * a path of `/`.
    */
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+  def index = Action { implicit request =>
+
+    val input = request.body
+
+    val outputStream = new ByteArrayOutputStream
+    val pdf = Pdf(new PdfConfig {
+      orientation := Portrait
+      pageSize := "Letter"
+      marginTop := "0.5cm"
+      marginBottom := "0.5cm"
+      marginLeft := "0.5cm"
+      marginRight := "0.5cm"
+      printMediaType := Some(true)
+    })
+
+    Logger.info("build input stream")
+    val content = new ByteArrayInputStream(input.getBytes("UTF-8"))
+    Logger.info("build input stream done")
+
+    Logger.info("running")
+    pdf.run(content, outputStream)
+
+    Ok(outputStream).as("application/pdf")
   }
 
 }
--- build.sbt
+++ build.sbt
@@ -7,9 +7,5 @@ lazy val root = (project in file(".")).enablePlugins(PlayScala)
 scalaVersion := "2.11.7"
 
 libraryDependencies ++= Seq(
-  jdbc,
-  cache,
-  ws,
-  "org.scalatestplus.play" %% "scalatestplus-play" % "1.5.1" % Test
+  "io.github.cloudify" %% "spdf" % "1.3.1"
 )
-
